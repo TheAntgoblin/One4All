aa939eeb916eda292edb9ca3d560f4e386751942
TheAntgoblin <antgoblin@gmail.com>
2026-07-07 15:47:15 +0300

    Early Drafts

---
+++ All4one.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEnNn3hthsR89evC_MHn6knpqm55J96uI",
+  authDomain: "all4one-1ca59.firebaseapp.com",
+  projectId: "all4one-1ca59",
+  storageBucket: "all4one-1ca59.firebasestorage.app",
+  messagingSenderId: "736453021564",
+  appId: "1:736453021564:web:889e2b3eb8f752309c9ef9",
+  measurementId: "G-Y6CZCQSC8W"
+};
